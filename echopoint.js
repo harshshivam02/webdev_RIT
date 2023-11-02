466990df3d0cd41eca3ac1b39192f0c65d5f70fa
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2023-11-02 17:10:24 +0530

    Delete echopoint.js

--- echopoint.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter=require("events");
-const shoutEvent=new EventEmitter();
-shoutEvent.on('echoPoint', function(data){
-    console.log("Custom event received : ", data);
-})
-
-shoutEvent.emit("echoPoint", {message: "Shouting name on echo point"})
